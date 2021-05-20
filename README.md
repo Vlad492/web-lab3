3030dea9e64826366b585a80cc9ef46d1213cff5
vlad.hleb <vladyslav.hleb@gen.tech>
2021-05-20 18:50:15 +0300

    added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+Глеб Владислав, КВ83, Лабораторна робота 1 ВЕБ <br/>
+Google Drive: https://docs.google.com/document/d/1vhgq7-wMFyd7Mp-t7I0hKpCwdTAdtT46
--- front/view/view.js
+++ front/view/view.js
@@ -15,23 +15,6 @@ class ContactsView {
                 contacts: [...contactsStore.getState()]
             }
         })
-        // state.forEach((elem, index) => {
-        //     let block = document.createElement("tr");
-        //     block.innerHTML = `<th scope="row">${index}</th><td>${elem.name}</td><td>${elem.number}</td><td><button type="button" class="btn btn-danger contacts-delete tmp-data" data="${index}">Delete contect</button></td>`
-        //     if(document.getElementById('contact-table')){
-        //         document.getElementById('contact-table').appendChild(block);
-        //     }
-        //     let buttons = document.getElementsByClassName('tmp-data')
-        //     if(buttons.length){
-        //         buttons[0].addEventListener('click', (e) => {
-        //             contactsStore.dispatch({
-        //                 type:'REMOVE_CONTACT',
-        //                 id:parseInt(e.target.attributes.data.nodeValue)
-        //             })
-        //         })
-        //     buttons[0].classList.remove('tmp-data')
-        //     }
-        // })
 
     }
 }
